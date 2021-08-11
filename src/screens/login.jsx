1e7ad30d03ca49aafdbf5b3aa2a9057d9c24d2a7
gerssivaldosantos <canalandows@gmail.com>
2021-08-11 15:21:44 -0300

    layout finalized

--- src/screens/login.jsx
+++ src/screens/login.jsx
@@ -1,21 +1,68 @@
 import React from 'react';
-import { Text, View,StatusBar, Image, TextInput } from 'react-native';
+import { Text, View, StatusBar, Image, TextInput, TouchableOpacity } from 'react-native';
 import styles from '../styles/login';
 
-export default function Login(){
-  return(
+export default function Login() {
+  return (
     <View style={styles.container}>
-    <StatusBar backgroundColor="#fff" barStyle="dark-content"></StatusBar>
-    <View style={styles.languageContainer}>
-    <Text style={styles.languageTitle}>English (United States)</Text>
-    </View>
-    <View style={styles.midContainer}>
+      <StatusBar backgroundColor="#fff" barStyle="dark-content"></StatusBar>
+      <View style={styles.languageContainer}>
+        <Text style={styles.languageTitle}>English (United States)</Text>
+      </View>
+      <View style={styles.midContainer}>
         <Image source={require("../assets/logo.png")} />
-        <TextInput 
-        
-        style={styles.input}
-        placeholder={"Phone number, email or username"} />
-    </View>
+        <TextInput
+
+          style={styles.input}
+          placeholder={"Phone number, email or username"} />
+        <TextInput
+
+          style={styles.input}
+          placeholder={"Password"} />
+        <TouchableOpacity style={styles.logButton}>
+          <Text style={styles.logText}>Log In</Text>
+        </TouchableOpacity>
+        <View style={styles.forgotContainer}>
+          <Text style={styles.smallText}>Forgot your login details?</Text>
+          <TouchableOpacity
+            style={styles.forgotButton}
+          >
+            <Text style={styles.smallButtonText}> Get help logging in.</Text>
+          </TouchableOpacity>
+
+        </View>
+
+        <View style={styles.orContainer}>
+          <View style={styles.line}></View>
+          <Text style={styles.orText}>OR</Text>
+          <View style={styles.line}></View>
+        </View>
+        <TouchableOpacity style={styles.logFacebookContainer}>
+          <View style={styles.logoContainer}><Text style={styles.logoText}>f</Text></View>
+          <Text style={styles.logFacebookText}>Log in with Facebook</Text>
+        </TouchableOpacity>
+
+      </View>
+      <View style={styles.footerContainer}>
+        <View style={styles.line2} />
+        <View style={{
+
+          flexDirection: "row",
+          alignItems: "center",
+          justifyContent: 'center',
+          marginTop:20
+        }}>
+          <Text style={styles.smallText}>Don`t have an account? </Text>
+
+          <TouchableOpacity>
+
+            <Text style={styles.smallButtonText}>Sign up.</Text>
+
+          </TouchableOpacity>
+
+        </View>
+
+      </View>
     </View>
   )
 }
--- src/styles/login.js
+++ src/styles/login.js
@@ -2,28 +2,124 @@ import React from 'react';
 import { StyleSheet } from 'react-native';
 
 const styles = StyleSheet.create({
-    container:{
-        flex:1,
-        backgroundColor:"#fff"
+    container: {
+        flex: 1,
+        backgroundColor: "#fff",
+        justifyContent:"space-between"
     },
-    languageContainer:{
-       
+    languageContainer: {
+
         height: 50,
-        justifyContent:'center'
+        justifyContent: 'center'
     },
-    languageTitle:{
-        color:"#7C7C7C",
-        textAlign:'center',
+    languageTitle: {
+        color: "#9C9C9C",
+        textAlign: 'center',
         fontSize: 16.5
     },
-    midContainer:{
-       padding:32.5, 
-       alignItems:'center'
+    midContainer: {
+        padding: 32.5,
+        alignItems: 'center',
+        justifyContent: 'center'
     },
-    input:{
-        borderStyle:"solid",
-        borderColor:"#121212"
+    input: {
+        borderStyle: "solid",
+        borderColor: "#9999",
+        fontSize: 17.5,
+        borderWidth: 0.8,
+        borderRadius: 7,
+        padding: 16,
+        marginTop: 20,
+        width: "100%"
+    },
+    logButton: {
+        marginTop: 20,
+        backgroundColor: "#0095f6",
+        padding: 20,
+        width: "100%",
+        borderRadius: 7
+    },
+    logText: {
+        textAlign: 'center',
+        fontSize: 18.5,
+        fontWeight: 'bold',
+        color: "white"
+    },
+    forgotContainer: {
+        marginTop: 20,
+        flexDirection: 'row'
+    },
+    smallText: {
+        color: "#9C9C9C",
+        fontSize: 15
+    },
+    forgotButton: {
+
+    },
+    smallButtonText: {
+        fontSize: 15,
+        color: "#00386b",
+        fontWeight: 'bold'
+    },
+    orContainer: {
+
+        marginTop: 10,
+        flexDirection: "row",
+        justifyContent: 'center',
+        alignItems: 'center',
+        width: "100%"
+    },
+    orText: {
+        padding: 10,
+        fontSize: 18,
+        fontWeight: 'bold',
+        color: "#11111158"
+    },
+    line: {
+
+        backgroundColor: "#80808060",
+        height: 1,
+        width: "43%"
+    },
+    logFacebookContainer:{
+        marginTop:20,
+        flexDirection:"row",
+        justifyContent:'center',
+        alignItems:'center'
+    },
+    logoContainer:{
+        
+        width:33,
+        height:33,
+        borderRadius:35,
+        backgroundColor:"#0095f6",
+        alignItems:'center',
+        justifyContent:'center'
+    },
+    logoText:{
+        marginTop:8,
+        color:"white",
+        fontSize:38,
+        fontWeight:'bold'
+    },
+    logFacebookText:{
+        marginLeft:7,
+        color:"#0095f6",
+        fontSize:18,
+        fontWeight:'bold'
+    },
+    footerContainer:{
+        height:"10%",
+        width:"100%",
+        
+    },
+    line2: {
+
+        backgroundColor: "#80808085",
+        height: 1,
+        width: "100%"
     }
+
 
 })
 
